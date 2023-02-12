4cd6df593160f8f0f2686a072b37c0ef4db1d3a5
Venia <vsollery@gmail.com>
2023-02-12 13:41:48 +0700

    add reset form when added

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -21,6 +21,10 @@ function CreateArea(props) {
 
     function submitNotes(event){
         props.onAdd(note);
+        setNotes({
+            title: "",
+            content: ""
+        })
         event.preventDefault();
     }
 
